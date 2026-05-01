2b67208f7c28ecab9ebed708ddb9407278796287
rowinile <fbberatgok757@gmail.com>
2026-05-01 15:30:05 +0300

    Update image paths for levels in constructors.js

--- js/constructors.js
+++ js/constructors.js
@@ -44,7 +44,7 @@ const levels = [
     "94%, 85%",
     "28-31, 40-50, 57-60, 65, 78",
     "0-28, 81-100",
-    "../img/midnight.png",
+    "rowinile-site/img/midnight.png",
   ),
   new Level(
     "Starlight Summit by hazelmrow",
@@ -59,7 +59,7 @@ const levels = [
     "80% x2, 78%",
     "the level",
     "",
-    "../img/starlight.png",
+    "rowinile-site/img/starlight.png",
   ),
   new Level(
     "Starlit Stroll by Enfur",
@@ -74,7 +74,7 @@ const levels = [
     "100%, 86%, 79%",
     "35-45, 60-68",
     "0-11, 68-76",
-    "../img/starlit.png",
+    "rowinile-site/img/starlit.png",
   ),
   new Level(
     "Sky Shredder by dorvict",
@@ -89,7 +89,7 @@ const levels = [
     "86%, 77%, 70%",
     "18-23, 27, 39-52, 70-86",
     "9-15, 52-70",
-    "../img/skyShredder.png",
+    "rowinile-site/img/skyShredder.png",
   ),
   new Level(
     "RUST by neigefeu",
@@ -104,7 +104,7 @@ const levels = [
     "95% x3, 94%, 88%, 86% x2",
     "0-17, 24-27, 76-100",
     "43-76",
-    "../img/rust.png",
+    "rowinile-site/img/rust.png",
   ),
   new Level(
     "Lucid Nightmares by CairoX",
@@ -119,7 +119,7 @@ const levels = [
     "90%, 75% x4, 74% x2, 72% x3",
     "2-7, 50-66, 72-75",
     "40-50, 7-18",
-    "../img/lucidNightmares.png",
+    "rowinile-site/img/lucidNightmares.png",
   ),
   new Level(
     "The Rupture by Ka1ns",
@@ -134,7 +134,7 @@ const levels = [
     "71% x3, 70%, 62% x2",
     "11-13, 34-36, 45-50, 69-72",
     "18-27, 30-34, 54-65, 81-100",
-    "../img/theRupture.png",
+    "rowinile-site/img/theRupture.png",
   ),
   new Level(
     "Sazerix by Zeptrus",
@@ -149,7 +149,7 @@ const levels = [
     "76%, 71%, 70%",
     "17-32, 0-17 (in that order)",
     "50-66",
-    "../img/sazerix.png",
+    "rowinile-site/img/sazerix.png",
   ),
   new Level(
     "Endless Dream by DreamTide",
@@ -164,7 +164,7 @@ const levels = [
     "50%, 49%",
     "0-23, 32-34, 85-100",
     "23-32, 36-65",
-    "../img/endlessDream.png",
+    "rowinile-site/img/endlessDream.png",
   ),
   new Level(
     "Bloodbath by Riot",
@@ -179,7 +179,7 @@ const levels = [
     "N/A",
     "N/A",
     "N/A",
-    "../img/bloodbath.png",
+    "rowinile-site/img/bloodbath.png",
   ),
   new Level(
     "Hidden in The Sand",
@@ -194,7 +194,7 @@ const levels = [
     "N/A",
     "N/A",
     "N/A",
-    "../img/hiddenInTheSand.png",
+    "rowinile-site/img/hiddenInTheSand.png",
   ),
 ];
 
