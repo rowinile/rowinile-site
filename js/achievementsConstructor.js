f1456dd6ee2c68ec9bc4ed367d0ba0d3193e58a6
rowinile <fbberatgok757@gmail.com>
2026-05-12 18:26:35 +0300

    Update achievement percentage and name

--- js/achievementsConstructor.js
+++ js/achievementsConstructor.js
@@ -7,7 +7,7 @@ class Achievement {
 }
 
 const achievements = [
-  new Achievement("???", "72%, 15-92", ""),
+  new Achievement("???", "83%", ""),
   new Achievement("Solar Flare", "69%", "solarFlare.png"),
   new Achievement("Midnight", "100%", "midnight.png"),
   new Achievement("arcturus", "83%", "arcturus.png"),
